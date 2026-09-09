76fc405efbb2324fa9bba564ef5015bed1bfc594
ALI IBRAHIM M ALIBRAHIM <146109014+ayancybers@users.noreply.github.com>
2026-09-09 19:17:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
+
         const response = await fetch('/api/log', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ userAgent: navigator.userAgent })
         });
+        
         const data = await response.json();
+        console.گ("Server Response:", data); 
         
         if (data.success && data.visits) {
             const counterElement = document.getElementById('visitor-count');
